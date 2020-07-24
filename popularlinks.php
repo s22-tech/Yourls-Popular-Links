0da2fa170c7ec69423601fe3574f087347ddd2c5
s22-tech <59073912+s22-tech@users.noreply.github.com>
2020-07-24 09:35:49 -0700

    Update popularlinks.php

--- popularlinks.php
+++ popularlinks.php
@@ -1,9 +1,9 @@
 <?php
 
-// Make sure we're in YOURLS context
-if( !defined( 'YOURLS_ABSPATH' ) ) {
-	// Attempt to guess URL via YOURLS
-	$url = 'http://' . $_SERVER['HTTP_HOST'] . str_replace( array( '/pages/', '.php' ) , array ( '/', '' ), $_SERVER['REQUEST_URI'] );
+// Make sure we're in YOURLS context.
+if ( !defined( 'YOURLS_ABSPATH' ) ) {
+	// Attempt to guess URL via YOURLS.
+	$url = 'https://' . $_SERVER['HTTP_HOST'] . str_replace( array( '/pages/', '.php' ) , array ( '/', '' ), $_SERVER['REQUEST_URI'] );
 	echo "Try this instead: <a href='$url'>$url</a>";
 	die();
 }
@@ -13,7 +13,7 @@ $url = YOURLS_SITE . '/popularlinks';
 
 yourls_html_head( 'popularlinks', 'Popular Links' );
 
-// Start YOURLS engine
+// Start YOURLS engine.
 require_once $_SERVER['DOCUMENT_ROOT'].'/includes/load-yourls.php' ;
 
 
@@ -24,34 +24,37 @@ HTML;
 function show_top($numdays,$numlinks) {
 
 global $ydb;
- $base  = YOURLS_SITE;
+ $base = YOURLS_SITE;
  $table_url = YOURLS_DB_TABLE_URL;
 // 
-$links 	= '';
+$links = '';
 
-$query = $ydb->get_results("SELECT title, timestamp, url, keyword, clicks FROM `$table_url` WHERE timestamp >= SUBDATE(CURDATE(), $numdays) order by clicks desc limit $numlinks");
+$query = $ydb->fetchObjects("SELECT `title`, `timestamp`, `url`, `keyword`, `clicks` 
+                             FROM `$table_url` WHERE `timestamp` >= SUBDATE(CURDATE(), $numdays) 
+                             ORDER BY `clicks` DESC 
+                             LIMIT $numlinks");
 if ($query) {
-	foreach( $query as $query_result ) {
-		$thisURLArray 	= parse_url(stripslashes($query_result->url));
-		$diff = abs( time() - strtotime( $query_result->timestamp ) );
-	    $days = floor( $diff / (60*60*24) );
-	if( $days <1) {
+	foreach ( $query as $query_result ) {
+      $this_url_array = parse_url(stripslashes($query_result->url));
+      $diff = abs( time() - strtotime( $query_result->timestamp ) );
+      $days = floor( $diff / (60 * 60 * 24) );
+	if ( $days < 1) {
 		$created = 'today';
 	} 
-	else if( $days <2) {
+	else if ( $days < 2) {
 		$created = ' 1 day ago';
 	}
 	else  {
 		$created = $days. ' days ago';
 	}
-		$links 		.= '(' . $query_result->clicks . ')  - ' .$thisURLArray[host] .' - <a href="' . $base . '/' . $query_result->keyword .'" target="blank">';
+   $links .= '(' . $query_result->clicks . ')  - ' .$this_url_array[host] .' - <a href="' . $base . '/' . $query_result->keyword .'" target="blank">';
 	
-$links 		.= str_replace('www.', '', $query_result->title) . '</a> <a href=" ' . $base . '/' . $query_result->keyword .'+" target="blank"></a> Created ' . $created . '<br/>';
+   $links .= str_replace('www.', '', $query_result->title) . '</a> <a href=" ' . $base . '/' . $query_result->keyword .'+" target="blank"></a> Created ' . $created . '<br>';
 	}
 }
 echo '<h3><b>Popular Links in the Last '. $numdays . ' Days:</b></h3><br/> ' . $links . "<br><br>\n\r";
 }
-//edit these if you want to show a different number of days/number of links! i.e. 1,5 = 5 most popular links created in the last 1 day
+// Edit these if you want to show a different number of days/number of links! i.e. 1,5 = 5 most popular links created in the last 1 day.
 show_top(1,5);
 show_top(30,5);
 show_top(365,5);
